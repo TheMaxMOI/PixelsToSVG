728a29bdda541e723573aeb1bb881b34fbe6980d
Maximilien <maximilienmaison@orange.fr>
2026-08-11 12:16:18 +0200

    main: grep a few stuff to have a quick overview

---
+++ png/__init__.py
@@ -0,0 +1,3 @@
+from .convert import SVGtoPNG
+
+__all__ = ["SVGtoPNG"]
--- png/convert.py
+++ png/convert.py
@@ -2,29 +2,31 @@ from pathlib import Path
 
 import resvg_py
 
+DEFAULT_FOLD = "img_output"
 
-def toPNG(
+dest_fold = Path(DEFAULT_FOLD)
+if not dest_fold.exists():
+    raise NameError(f"The path {dest_fold} doesn't exist!")
+
+
+def SVGtoPNG(
     svg: str,
     name="output",
-    dest="img_output",
+    dest=dest_fold,
     targetSize: tuple[int, int] | None = None,
 ):
-    dir = Path(dest)
-    if not dir.exists():
-        raise NameError(f"The path {dir} doesn't exist!")
 
     theName = Path(name).with_suffix(".png")
-    theDest = dir / theName
+    theDest = dest / theName
 
     renderOpts = {}
     if targetSize is not None:
         renderOpts["width"], renderOpts["height"] = targetSize
 
-    theDest.write_bytes(
-        resvg_py.svg_to_bytes(svg_string=svg, **renderOpts)
-    )
+    theDest.write_bytes(resvg_py.svg_to_bytes(svg_string=svg, **renderOpts))
 
     return theDest
+
 
 # # On the fly example
 # svg='<?xml version="1.0" encoding="UTF-8"?>\n<svg viewBox="-1 -1 42 37" fill-opacity="0" width="420" height="370" xmlns="http://www.w3.org/2000/svg">\n    <polygon stroke="#ff0000" stroke-width="1" points="30.0,34.64 10.0,34.64 0,17.32 10.0,0 30.0,0 40,17.32"/></svg>'
